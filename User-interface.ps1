e5c25c9301c4f6dac9142d103a53929ec43efe95
CavaliereDavid <54360776+CavaliereDavid@users.noreply.github.com>
2023-07-28 17:17:17 +0200

    fixed issue by putting show menu as last

--- User-interface.ps1
+++ User-interface.ps1
@@ -1,85 +1,3 @@
-function MostraMenu {
-    Clear-Host
-    Write-Host "premi 1 per creare una nuova cartella"
-    Write-Host "premi 2 per creare un nuovo file di testo"
-    Write-Host "premi 3 per copiare una cartella"
-    Write-Host "premi 4 per copiare un file"
-    Write-Host "premi 5 per cancellare una cartella"
-    Write-Host "premi 6 per cancellare un file"
-    Write-Host "premi 7 per spostare una cartella"
-    Write-Host "premi 8 per spostare un file"
-    Write-Host "premi 9 per rinominare una cartella"
-    Write-Host "premi 10 per rinominare un file"
-    Write-Host "premi 11 per comprimere una cartella"
-    Write-Host "premi 12 per estrarre una cartella"
-    Write-Host "premi Q per uscire"
-}
-do {
-    MostraMenu
-    $scelta = Read-Host "fai una scelta: "
-    switch ($scelta) {
-        "1" {
-            $parentPath = Read-Host "Inserisci il percorso della cartella padre: "
-            $folderName = Read-Host "Inserisci il nome della nuova cartella: "
-            CreateFolder -ParentFolderPath $parentPath -NewFolderName $folderName
-        }
-        "2" {
-            $filePath = Read-Host "Inserisci il percorso dove creare il file: "
-            $content = Read-Host "Inserisci il testo del file: "
-            CreateTextFile -FilePath $filePath -Content $content
-        }
-        "3" {
-            $sourcePath = Read-Host "Inserisci il percorso della cartella da copiare: "
-            $destinationPath = Read-Host "Inserisci il percorso della cartella di destinazione: "
-            CopyFolder -SourceFolderPath $sourcePath -DestinationFolderPath $destinationPath
-        }
-        "4" {
-            $sourceFile = Read-Host "Inserisci il percorso del file da copiare: "
-            $destinationFile = Read-Host "Inserisci il percorso del file di destinazione: "
-            CopyFile -SourceFilePath $sourceFile -DestinationFilePath $destinationFile
-        }
-        "5" {
-            $folderToDelete = Read-Host "Inserisci il percorso della cartella da cancellare: "
-            DeleteFolder -FolderPath $folderToDelete
-        }
-        "6" {
-            $fileToDelete = Read-Host "Inserisci il percorso del file da cancellare: "
-            DeleteFile -FilePath $fileToDelete
-        }
-        "7" {
-            $sourcePath = Read-Host "Inserisci il percorso della cartella da spostare: "
-            $destinationParentPath = Read-Host "Inserisci il percorso della cartella padre di destinazione: "
-            MoveFolder -SourceFolderPath $sourcePath -DestinationParentFolderPath $destinationParentPath
-        }
-        "8" {
-            $sourceFile = Read-Host "Inserisci il percorso del file da spostare: "
-            $destinationParentPath = Read-Host "Inserisci il percorso della cartella padre di destinazione: "
-            MoveFile -SourceFilePath $sourceFile -DestinationParentFolderPath $destinationParentPath
-        }
-        "9" {
-            $folderPath = Read-Host "Inserisci il percorso della cartella da rinominare: "
-            $newName = Read-Host "Inserisci il nuovo nome della cartella: "
-            RenameFolder -FolderPath $folderPath -NewName $newName
-        }
-        "10" {
-            $filePath = Read-Host "Inserisci il percorso del file da rinominare: "
-            $newName = Read-Host "Inserisci il nuovo nome del file: "
-            RenameFile -FilePath $filePath -NewName $newName
-        }
-        "11" {
-            $folderToCompress = Read-Host "Inserisci il percorso della cartella da comprimere: "
-            CompressFolder -FolderPath $folderToCompress
-        }
-        "12" {
-            $archivePath = Read-Host "Inserisci il percorso dell'archivio compresso: "
-            $destinationPath = Read-Host "Inserisci il percorso di destinazione per l'estrazione: "
-            ExtractFolder -ArchivePath $archivePath -DestinationPath $destinationPath
-        }
-    }
-    Pause
-} until ($scelta -eq 'q')
-
-
 function CreateFolder {
     param (
         [string]$ParentFolderPath,
@@ -263,6 +181,83 @@ function ExtractFolder {
         Write-Host "Archive not found: $ArchivePath"
     }
 }
-
-
-       
+function MostraMenu {
+    Clear-Host
+    Write-Host "premi 1 per creare una nuova cartella"
+    Write-Host "premi 2 per creare un nuovo file di testo"
+    Write-Host "premi 3 per copiare una cartella"
+    Write-Host "premi 4 per copiare un file"
+    Write-Host "premi 5 per cancellare una cartella"
+    Write-Host "premi 6 per cancellare un file"
+    Write-Host "premi 7 per spostare una cartella"
+    Write-Host "premi 8 per spostare un file"
+    Write-Host "premi 9 per rinominare una cartella"
+    Write-Host "premi 10 per rinominare un file"
+    Write-Host "premi 11 per comprimere una cartella"
+    Write-Host "premi 12 per estrarre una cartella"
+    Write-Host "premi Q per uscire"
+}
+do {
+    MostraMenu
+    $scelta = Read-Host "fai una scelta: "
+    switch ($scelta) {
+        "1" {
+            $parentPath = Read-Host "Inserisci il percorso della cartella padre: "
+            $folderName = Read-Host "Inserisci il nome della nuova cartella: "
+            CreateFolder -ParentFolderPath $parentPath -NewFolderName $folderName
+        }
+        "2" {
+            $filePath = Read-Host "Inserisci il percorso dove creare il file: "
+            $content = Read-Host "Inserisci il testo del file: "
+            CreateTextFile -FilePath $filePath -Content $content
+        }
+        "3" {
+            $sourcePath = Read-Host "Inserisci il percorso della cartella da copiare: "
+            $destinationPath = Read-Host "Inserisci il percorso della cartella di destinazione: "
+            CopyFolder -SourceFolderPath $sourcePath -DestinationFolderPath $destinationPath
+        }
+        "4" {
+            $sourceFile = Read-Host "Inserisci il percorso del file da copiare: "
+            $destinationFile = Read-Host "Inserisci il percorso del file di destinazione: "
+            CopyFile -SourceFilePath $sourceFile -DestinationFilePath $destinationFile
+        }
+        "5" {
+            $folderToDelete = Read-Host "Inserisci il percorso della cartella da cancellare: "
+            DeleteFolder -FolderPath $folderToDelete
+        }
+        "6" {
+            $fileToDelete = Read-Host "Inserisci il percorso del file da cancellare: "
+            DeleteFile -FilePath $fileToDelete
+        }
+        "7" {
+            $sourcePath = Read-Host "Inserisci il percorso della cartella da spostare: "
+            $destinationParentPath = Read-Host "Inserisci il percorso della cartella padre di destinazione: "
+            MoveFolder -SourceFolderPath $sourcePath -DestinationParentFolderPath $destinationParentPath
+        }
+        "8" {
+            $sourceFile = Read-Host "Inserisci il percorso del file da spostare: "
+            $destinationParentPath = Read-Host "Inserisci il percorso della cartella padre di destinazione: "
+            MoveFile -SourceFilePath $sourceFile -DestinationParentFolderPath $destinationParentPath
+        }
+        "9" {
+            $folderPath = Read-Host "Inserisci il percorso della cartella da rinominare: "
+            $newName = Read-Host "Inserisci il nuovo nome della cartella: "
+            RenameFolder -FolderPath $folderPath -NewName $newName
+        }
+        "10" {
+            $filePath = Read-Host "Inserisci il percorso del file da rinominare: "
+            $newName = Read-Host "Inserisci il nuovo nome del file: "
+            RenameFile -FilePath $filePath -NewName $newName
+        }
+        "11" {
+            $folderToCompress = Read-Host "Inserisci il percorso della cartella da comprimere: "
+            CompressFolder -FolderPath $folderToCompress
+        }
+        "12" {
+            $archivePath = Read-Host "Inserisci il percorso dell'archivio compresso: "
+            $destinationPath = Read-Host "Inserisci il percorso di destinazione per l'estrazione: "
+            ExtractFolder -ArchivePath $archivePath -DestinationPath $destinationPath
+        }
+    }
+    Pause
+} until ($scelta -eq 'q')
